62dc7e9a7e77e35d3f33d8ad468024e222df0842
yuto ichinose <yuto.0526.sano@gmail.com>
2023-02-01 13:26:18 +0900

    fisrt commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Center,
+  ChakraProvider,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+// import './App.css'
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  useEffect(() => {
+    console.log(isOpen);
+  }, [isOpen]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Center w="100vw" h="100vh" className="App">
+        <Button onClick={onOpen}>Open Modal</Button>
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text>Modal Body</Text>
+              <Popover>
+                <PopoverTrigger>
+                  <Button>Trigger</Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <PopoverArrow />
+                  <PopoverCloseButton />
+                  <PopoverHeader>Popover Header</PopoverHeader>
+                  <PopoverBody>
+                    Popover Body
+                  </PopoverBody>
+                </PopoverContent>
+              </Popover>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant="ghost">Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Center>
+    </ChakraProvider>
   );
 }
 
